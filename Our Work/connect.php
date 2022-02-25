7bb0591582e6483cda75c9aa9e9cea6701019a4f
Pooritha <95603205+Pooritha@users.noreply.github.com>
2022-02-25 08:46:26 +0530

    Create connect.php

---
+++ Our Work/connect.php
@@ -0,0 +1,15 @@
+<?php
+
+    $firstname = $_POST['firstname'];
+    $email = $_POST['email'];
+    $address = $_POST['address'];
+    $city = $_POST['city'];
+    $state = $_POST['state'];
+    $zip = $_POST['zip'];
+    $cardname = $_POST['cardname'];
+    $cardnumber = $_POST['cardnumber'];
+    $expmonth = $_POST['expmonth'];
+    $expyear = $_POST['expyear'];
+    $cvv = $_POST['cvv'];
+    $Amount = $_POST['Amount'];
+  
